18070e4de6d97f13871378bc8a924ac80a74498e
AhmedFatir <afatir.ahmedfatir@gmail.com>
2025-02-01 15:00:22 +0100

    chore: Add .gitignore for Terraform/Nginx directory and update main.tf

--- Terraform/Nginx/main.tf
+++ Terraform/Nginx/main.tf
@@ -1,6 +1,16 @@
+variable "port_internal" {
+  description = "value of the internal port"
+  default     = 0
+  type        = number
+  validation {
+    condition     = var.port_internal > 0
+    error_message = "Internal Port must be greater than 0"
+  }
+}
+
 module "nginx" {
   source        = "./nginx-native"
   port_internal = 80
-  port_external = 8000
+  port_external = var.port_internal
   docker_user   = "afatir"
 }
